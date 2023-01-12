d6b34b436367626a9e4fc82671f995c90739eef6
wzhou08817 <wzhou.08817@gmail.com>
2023-01-12 17:15:26 -0500

    update

--- create_conf_file.sh
+++ create_conf_file.sh
@@ -14,6 +14,6 @@ cd resource-gist
 git config --global user.email "wzhou.08817@gmail.com"
 git config --global user.name "wzhou08817"
 
-git add .
-git commit * -m "backhaul config file"
+#git add .
+git commit backhaul-switch.conf -m "backhaul_config_file"
 
