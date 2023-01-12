6cb7d3d16bbbd44fd759361cc95be0f40c5db6d8
Wei Zhou <wei_zhou@comcast.com>
2023-01-11 23:45:58 -0500

    add shell script

---
+++ create_conf_file.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e # fail fast
+set -x # print commands
+
+apt update
+apt upgrade -y
+apt install git -y
+
+git clone resource-generator
+cd resource-generator
+ansible-playbook main.yml -e "@configs/backhaul_switch_config.yml"
+cd ..
+
+git clone resource-gist updated-gist
+cp resource-generator/backhaul-switch.conf updated-gist/backhaul-switch.conf
+
+cd updated-gist
+
+git config --global user.email "wzhou.08817@gmail.com"
+git config --global user.name "wzhou08817"
+
+git add .
+git commit -m "backhaul config file"
+
