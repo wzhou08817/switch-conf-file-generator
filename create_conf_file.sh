9a67f7d09be936f8c696529963ceaa2895e27572
wzhou08817 <wzhou.08817@gmail.com>
2023-01-12 16:55:03 -0500

    update

--- create_conf_file.sh
+++ create_conf_file.sh
@@ -3,19 +3,17 @@
 set -e # fail fast
 set -x # print commands
 
-#git clone resource-generator
 cd resource-generator
 ansible-playbook main.yml -e "@configs/backhaul_switch_config.yml"
 cd ..
 
-#git clone resource-gist updated-gist
-cp resource-generator/backhaul-switch.conf updated-gist/backhaul-switch.conf
+cp resource-generator/backhaul-switch.conf resource-gist/backhaul-switch.conf
 
-cd updated-gist
+cd resource-gist
 
 git config --global user.email "wzhou.08817@gmail.com"
 git config --global user.name "wzhou08817"
 
 git add .
-git commit -m "backhaul config file"
+git commit * -m "backhaul config file"
 
