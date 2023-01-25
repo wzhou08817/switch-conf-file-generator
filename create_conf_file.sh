cfdc7d2dc686771adca90799dce9b4717e3abb8f
wzhou08817 <wzhou.08817@gmail.com>
2023-01-25 09:36:59 -0500

    for test

--- create_conf_file.sh
+++ create_conf_file.sh
@@ -18,7 +18,6 @@ date >> backhaul-switch.conf
 git config --global user.email "wzhou.08817@gmail.com"
 git config --global user.name "wzhou08817"
 
-
 git add .
 git commit -m "backhaul_config_updated"
 
